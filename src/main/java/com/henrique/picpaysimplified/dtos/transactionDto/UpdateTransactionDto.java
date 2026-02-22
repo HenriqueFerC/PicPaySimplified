43ab812f2ead627c6521602f4d692f760916d4a9
HenriqueFerC <henriqueferreiracardoso179411@hotmail.com>
2026-02-21 21:16:37 -0300

    revert transactions added, user transaction list, verification to transaction to shopkeepers not allowed to make transfers.

--- src/main/java/com/henrique/picpaysimplified/dtos/transactionDto/UpdateTransactionDto.java
+++
@@ -1,6 +0,0 @@
-package com.henrique.picpaysimplified.dtos.transactionDto;
-
-import com.henrique.picpaysimplified.model.Consistency;
-
-public record UpdateTransactionDto(Integer id, Consistency consistency) {
-}
--- src/main/java/com/henrique/picpaysimplified/repository/TransactionRepository.java
+++ src/main/java/com/henrique/picpaysimplified/repository/TransactionRepository.java
@@ -1,6 +1,7 @@
 package com.henrique.picpaysimplified.repository;
 
 import com.henrique.picpaysimplified.model.Transaction;
+import com.henrique.picpaysimplified.model.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -8,4 +9,5 @@ import java.time.LocalDateTime;
 
 public interface TransactionRepository extends JpaRepository<Transaction, Integer> {
     Page<Transaction> findByTransactionDateBetween(LocalDateTime startDate, LocalDateTime endDate, Pageable pageable);
+    Page<Transaction> findAllByPayer(User payer, Pageable pageable);
 }
